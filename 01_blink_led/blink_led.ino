67912a7f62d0d35594089c1e7bb943efc73aded5
33Subhash <128284988+33Subhash@users.noreply.github.com>
2025-07-08 14:27:39 +0530

    Update blink_led.ino

--- 01_blink_led/blink_led.ino
+++ 01_blink_led/blink_led.ino
@@ -1,17 +1,14 @@
+/*code to blink led */
 
 void setup()
-{
-  pinMode(2, OUTPUT);
-
+{  
+  pinMode(2, OUTPUT); //Set the pin direction  
 }
-
-
 void loop()
 {
-
+    digitalWrite(2,HIGH); 
+    delay(500); //delay in milliseconds 
     digitalWrite(2,HIGH);
-    delay(500);
-    digitalWrite(2,HIGH);
-    delay(500);
-
+    delay(500); //delay in milliseconds 
 }
+
